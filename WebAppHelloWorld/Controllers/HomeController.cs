53eb10983604b2b4e8dfb6462b6b72d8ce5912d0
djl <djl@DESKTOP-F5IDQLS>
2016-08-12 11:34:59 +0800

    表格 分页 搜索

--- WebAppHelloWorld/Controllers/HomeController.cs
+++ WebAppHelloWorld/Controllers/HomeController.cs
@@ -13,5 +13,32 @@ namespace WebAppHelloWorld.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public JsonResult GetDepartment(int limit, int offset, string departmentname, string statu)
+        {
+            var lstRes = new List<Department>();
+            for (var i = 0; i < 100; i++)
+            {
+                var oModel = new Department();
+                oModel.ID = Guid.NewGuid().ToString();
+                oModel.Name = "销售部" + i;
+                oModel.Level = i.ToString();
+                oModel.Desc = "暂无描述信息";
+                lstRes.Add(oModel);
+            }
+            lstRes = lstRes.Where(x => x.Name.Contains(departmentname)).Where(x => x.Desc.Contains(statu)).ToList();
+            var total = lstRes.Count;
+            var rows = lstRes.Skip(offset).Take(limit).ToList();
+            return Json(new { total = total, rows = rows }, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+    internal class Department
+    {
+        public string Desc { get; internal set; }
+        public string ID { get; internal set; }
+        public string Level { get; internal set; }
+        public string Name { get; internal set; }
     }
 }
