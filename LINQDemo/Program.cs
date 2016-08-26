80863bf0b862d1a2688dd1d5f145607778320795
djl <djl@DESKTOP-F5IDQLS>
2016-08-26 12:04:49 +0800

    linq

---
+++ LINQDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = new[] { "the", "quick", "brown", "fox", "jumps" };
+            var query = from word in words
+                        orderby word.Length
+                        select word;
+
+            foreach (var word in query)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
